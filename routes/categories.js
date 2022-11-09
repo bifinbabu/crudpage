bca422853c6fbf5c83c441ac34971a5adfa36746
bifinbabu <bifinbabu088@gmail.com>
2022-11-09 20:51:18 +0530

    Finished Category Routes

--- routes/categories.js
+++ routes/categories.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Category = require('../models/category')
+const Product = require('../models/product')
 const router = express.Router()
 
 // All Categories Route
@@ -24,13 +25,68 @@ router.post('/', async (req, res) =>{
     })
     try {
         const newCategory = await category.save()
-        // res.redirect(`categories/${newCategory.id}`)
-        res.redirect('categories')
+        res.redirect(`/categories/${newCategory.id}`)
     } catch {
         res.render('categories/new', { 
             category: category,
             errorMessage: 'Error creating category'
         })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id)
+        const products = await Product.find({ category: category.id }).exec() 
+        res.render('categories/show', {
+            category: category,
+            productsUnderCategory: products
+        })
+    } catch {
+        res.redirect('/')
+    }
+})
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id)
+        res.render('categories/edit', { category: category })
+    } catch {
+        res.redirect('/categories')
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    let category
+    try {
+        category = await Category.findById(req.params.id)
+        category.name = req.body.name
+        await category.save()
+        res.redirect(`/categories/${category.id}`)
+    } catch {
+        if (category == null) {
+            res.redirect('/')
+        } else {
+            res.render('categories/edit', { 
+                category: category,
+                errorMessage: 'Error updating category'
+            })
+        }
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    let category
+    try {
+        category = await Category.findById(req.params.id)
+        await category.remove()
+        res.redirect('/categories')
+    } catch {
+        if (category == null) {
+            res.redirect('/')
+        } else {
+            res.redirect(`/categories/${category.id}`)
+        }
     }
 })
 
